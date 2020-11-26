aa9a97fe7ee1a2b8c2c2a8a8c265daf0ff297f42
Dafna Pundak <dafna.m88@gmail.com>
2020-11-26 14:36:19 +0200

    linking projects and a bit styling

--- pages/index.js
+++ pages/index.js
@@ -13,49 +13,41 @@ export default function Home() {
         <h1 className={styles.title}>Welcome to my portfolio!</h1>
 
         <p className={styles.description}>
-         <p>I am Dafna Pundak, a full-stack developer based in Tel Aviv.</p>
-         <p>Take a look on my recent projects</p>
+          <p>I am Dafna Pundak, a full-stack developer based in Tel Aviv.</p>
+          <p>Take a look on my recent projects:</p>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://bank-app-dafna.herokuapp.com/" className={styles.card}>
+          <a
+            href="https://bank-app-dafna.herokuapp.com/"
+            className={styles.card}
+          >
             <h3>D-bank &rarr;</h3>
-            <p>Managing your money transactions, track your balance and helpping you plan future expenses.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Manage your money transactions, track your balance and plan future
+              expenses.
             </p>
+          </a>
+
+          <a
+            href="https://weather-app-dafna.herokuapp.com/"
+            className={styles.card}
+          >
+            <h3>Weather App &rarr;</h3>
+            <p>Check the weather in your next destination!</p>
+            <br />
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/DafnaPundak"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          View on{" "}
+          <img src="/github-icon.ico" alt="github Logo" className="logo" />
         </a>
       </footer>
     </div>
